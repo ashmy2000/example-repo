f3395123f28f41c0470c4e60ff3143f241c0a7dc
ashmy2000 <ashmy2000@hotmail.com>
2024-05-16 18:49:46 +0100

    latest update

--- exception/intro.py
+++ exception/intro.py
@@ -4,8 +4,25 @@
 
 try:
     num = int(input("Enter Num: "))
-    print(f"Inverse of {num} is {1/num}")
+    print(f"Inverse of {num} is {1/num}", num)
+    # raise SyntaxError
+except TypeError:
+    print("SyntaxError")
 except ValueError:
     print("ValueError")
 except ZeroDivisionError:
     print("ZeroDivisionError")
+
+"""
+BaseException
+    Exception   System Exit     KeyboardInterrupt
+    Arithmetic              LookupEroor TypeError ValueError
+    Zero DivisionalError    IndexError
+                            KeyError  
+
+
+
+
+"""
+
+
