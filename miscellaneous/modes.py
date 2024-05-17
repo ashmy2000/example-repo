c0b1703af2aaa714bf79899392a2d4be4e53e359
ashmy2000 <ashmy2000@hotmail.com>
2024-05-17 21:23:03 +0100

    new

---
+++ miscellaneous/modes.py
@@ -0,0 +1,10 @@
+"""
+r = open for reading
+w = open for writing, truncating the file first
+x = open for exclusive creation, fails if the file already exists
+a = open for writing, appending to the end of the file
+b = binary mode
+t = text mode
++ = open for updating (reading and writing)
+
+"""
---
+++ miscellaneous/predefined.py
@@ -0,0 +1,7 @@
+import sys
+# sees what entered in console
+for line in sys.stdin:
+    if line.rstrip() == "q":
+        break
+    print(line)
+print("you pressed q so break")
